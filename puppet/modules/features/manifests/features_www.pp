fc189a608fdbcd9192a725dcd7e66ee0e56d7427
Darren Gordon <web@darrengordon.net>
2013-09-07 18:25:32 +0000

    WWW: Added default mods to apache

--- puppet/modules/features/manifests/features_www.pp
+++ puppet/modules/features/manifests/features_www.pp
@@ -1,15 +1,15 @@
-# TODO: Include Features::PHP
-class features::features_www ( $domain, $docroot) {
+class features::features_www (
+	$domain,
+	$docroot
+) {
 	class { 'apache':
-		# default_mods  => ['rewrite', 'alias', 'auth_digest', 'authn_file', 'headers', 'mime', 'deflate', 'env'],
-		# 'cgi', 'deflate',
+		default_mods  => ['rewrite', 'alias', 'auth_digest', 'authn_file', 'headers', 'mime', 'deflate', 'env'],
 	}
 
 	apache::vhost { "www.${domain}":
 		vhost_name    => '*',
 		port          => '80',
 		docroot       => $docroot,
-		servername    => "www.${domain}",
-		serveraliases => ["admin.${domain}", "app.${domain}"]
+		servername    => "www.${domain}"
 	}
 }

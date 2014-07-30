352a225fb46ac7ebdd7178cfdc94c5beeac28dd8
Darren Gordon <dgordon@timesofmalta.com>
2014-07-30 20:13:04 +0700

    PHP: Only use composer, phpinfo and display_errors when debug

--- puppet/modules/features/manifests/features_php.pp
+++ puppet/modules/features/manifests/features_php.pp
@@ -24,18 +24,35 @@ class features::features_php (
     }
 
     php::module { [ 'curl', 'memcache', 'mysql', 'json' ]: }
-    
+
     php::module { 'apc':
         module_prefix => 'php-'
     }
-    
+
     if $debug {
         php::module { 'xdebug': }
+        class { 'composer':
+            command_name    => 'composer',
+            target_dir      => '/usr/local/bin'
+        }
+
+        if $docroot and $docroot != 'false' {
+            file { "${docroot}/phpinfo.php":
+                content => "<?php\nphpinfo();"
+            }
+        }
+    }
+
+    if str2bool("$debug") {
+        $display_errors = "On"
+    }
+    else {
+        $display_errors = "Off"
     }
 
     php::ini { 'php':
         value   => [
-            'display_errors = "On"',
+            "display_errors = \"${display_errors}\"",
             'log_errors = "On"',
             'error_reporting = E_ALL',
             "include_path = \".:/usr/share/php:./includes:${docroot}/includes\"",
@@ -44,16 +61,5 @@ class features::features_php (
         ],
         target  => 'php.ini',
         notify  => Class['apache']
-    }
-    
-    class { 'composer':
-        command_name    => 'composer',
-        target_dir      => '/usr/local/bin'
-    }
-
-    if $docroot and $docroot != 'false' {
-        file { "${docroot}/phpinfo.php":
-            content => "<?php\nphpinfo();"
-        }
     }
 }

c79401ed059d5f4e3951d8bfe7ab78346f9e5e06
Darren Gordon <dgordon@timesofmalta.com>
2014-12-12 20:05:31 +0700

    PHP: Install phpunit

--- puppet/modules/features/manifests/features_phptest.pp
+++ puppet/modules/features/manifests/features_phptest.pp
@@ -1,7 +1,18 @@
-# TODO: Include PHP
-# TODO: Install PHP unit
-class features::features_phptest {
-    file { '/tmp/phptest':
-        ensure  => exists
-    }
+class features::features_phptest (
+    $phar_uri = 'https://phar.phpunit.de/phpunit.phar',
+    $install_path = '/usr/local/bin/phpunit'
+) {
+      include wget
+
+      wget::fetch { 'phpunit-phar-wget':
+        source      => $phar_uri,
+        destination => $install_path,
+        verbose     => false
+      }
+      ->
+      file { 'phpunit-phar':
+        ensure  => 'present',
+        mode    => 'a+x',
+        path    => $install_path
+      }
 }
